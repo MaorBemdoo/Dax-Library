3fb59d43ae806e700c8b149707f03bb7d10b8a4f
Bemdoo Maor <bemdoo.maor1@gmail.com>
2024-07-28 22:46:39 +0100

    update dbt tables name

--- app/models/book.py
+++ app/models/book.py
@@ -1,11 +1,12 @@
 from app import db
 
 class Book(db.Model):
+    __tablename__ = 'books'
     id = db.Column(db.Integer, primary_key=True)
     author = db.Column(db.String(30), nullable=False)
     title = db.Column(db.String(64))
     content = db.Column(db.String(1024))
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
 
     def __repr__(self):
         return f'{self}'
--- app/models/user.py
+++ app/models/user.py
@@ -3,6 +3,7 @@ from app import db
 from datetime import datetime
 
 class User(db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     full_name = db.Column(db.String(30), nullable=False)
     username = db.Column(db.String(30), index=True, unique=True, nullable=False)
