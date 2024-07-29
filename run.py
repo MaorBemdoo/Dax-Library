b02ae38e80ec01c6a72a1b6936f800852a6cbb4f
Bemdoo Maor <bemdoo.maor1@gmail.com>
2024-07-29 21:24:04 +0100

    add port opt

--- run.py
+++ run.py
@@ -4,4 +4,4 @@ from dotenv import get_key
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=get_key(".env", "PYTHON_ENV") == "development")
+    app.run(debug=get_key(".env", "PYTHON_ENV") == "development", port=5000)
