be93a38b6b8a386f31b78fbf78bd7a848065e1f7
Bemdoo Maor <bemdoo.maor1@gmail.com>
2024-08-03 11:39:24 +0100

    fix

--- app/static/js/search.js
+++ app/static/js/search.js
@@ -1,5 +1,51 @@
 const booksContainer = document.getElementById("gridContainer")
 const searchBar = document.getElementById("searchBar")
+const logoutBtn = document.getElementById("logout")
+const modal = document.getElementById("modal")
+const alertDiv = document.getElementById("alert")
+
+const activateAndDeactivateAlert = (text, type) => {
+    alertDiv.innerText = text
+    alertDiv.style.backgroundColor = type == "success" ? "green" : type == "error" ? "red" : "blue"
+    alertDiv.classList.add("active")
+    modal.style.display = "none"
+    setTimeout(() => {
+        alertDiv.classList.remove("active")
+    }, 5000)
+}
+
+modal.addEventListener("click", (e) => {
+    modal.style.display = "none"
+})
+
+modal.childNodes.forEach(child => {
+    child.addEventListener("click", (e) => {
+        e.stopPropagation()
+    })
+})
+
+modal.querySelectorAll(".cancel").forEach(btn => {
+    btn.addEventListener("click", (e) => {
+        modal.style.display = "none"
+    })
+})
+
+modal.querySelector(".logout button").addEventListener("click", (e) => {
+    fetch("/api/logout").then(res => {
+        return res.json()
+    }).then(data => {
+        activateAndDeactivateAlert(data.message, "success")
+        location.assign("/login")
+    }).catch(err => {
+        console.log(err)
+        activateAndDeactivateAlert(err.message, "error")
+    })
+})
+
+logoutBtn.addEventListener("click", (e) => {
+    modal.style.display = "grid"
+    modal.getElementsByClassName("logout")[0].style.display = "grid"
+})
 
 booksContainer.innerHTML = `
     <div class="loading-placeholder"></div>
@@ -71,4 +117,3 @@ searchBar.addEventListener("keyup", (e) => {
             console.log(err)
         })
 })
-
