16f71f4ac15136d358e27eae55a288c7a23d19ef
emonzz <adebayo-.imonemini@binghamuni.edu.ng>
2024-08-04 19:04:12 +0100

    work on login and register

--- app/static/js/login.js
+++ app/static/js/login.js
@@ -1,13 +1,45 @@
-document.getElementById('sign-in-btn').addEventListener('click', (e) => {
+const alertDiv = document.getElementById("alert")
+
+const activateAndDeactivateAlert = (text, type) => {
+    alertDiv.innerText = text
+    alertDiv.style.backgroundColor = type == "success" ? "green" : type == "error" ? "red" : "blue"
+    alertDiv.classList.add("active")
+    setTimeout(() => {
+        alertDiv.classList.remove("active")
+    }, 5000)
+}
+
+document.getElementById('sign-in-btn').addEventListener('click', async(e) => {
     e.preventDefault();
     const username = document.getElementById('sign-in-username').value;
     const password = document.getElementById('sign-in-password').value;
 
     if (username === '' || password === '') {
-    alert('Please fill in both fields');
-    return;
+        activateAndDeactivateAlert('Please fill in both fields', "error");
+        return;
     }
 
-    alert('Sign In successful');
+    try{
+        const res = await fetch("/api/login", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+              username, password
+            })
+        })
+    
+        const data = await res.json()
+
+        if(!res.ok){
+            throw data.message
+        }
+    
+        location.assign("/")
+    }catch(err){
+        console.log(err)
+        activateAndDeactivateAlert(err, "error")
+    }
 });
   
--- app/static/js/register.js
+++ app/static/js/register.js
@@ -1,20 +1,73 @@
-document.getElementById('sign-up-btn').addEventListener('click', (e) => {
+const alertDiv = document.getElementById("alert")
+
+const activateAndDeactivateAlert = (text, type) => {
+    alertDiv.innerText = text
+    alertDiv.style.backgroundColor = type == "success" ? "green" : type == "error" ? "red" : "blue"
+    alertDiv.classList.add("active")
+    setTimeout(() => {
+        alertDiv.classList.remove("active")
+    }, 5000)
+}
+
+document.getElementById('sign-up-btn').addEventListener('click', async(e) => {
   e.preventDefault();
+  const full_name = document.getElementById('sign-up-fullname').value;
   const username = document.getElementById('sign-up-username').value;
   const password = document.getElementById('sign-up-password').value;
   const repeatPassword = document.getElementById('repeat-password').value;
-  const email = document.getElementById('email-address').value;
 
-  if (username === '' || password === '' || repeatPassword === '' || email === '') {
-    alert('Please fill in all fields');
+  if (username === '' || password === '' || repeatPassword === '' || full_name === '') {
+    activateAndDeactivateAlert("Please fill in required fields", "error")
+    return;
+  }
+
+  if(password <= 6){
+    activateAndDeactivateAlert("Passwords must be more than 6 characters", "error")
     return;
   }
 
   if (password !== repeatPassword) {
-    alert('Passwords do not match');
+    activateAndDeactivateAlert("Passwords do not match", "error")
     return;
   }
-  
-  alert('Sign Up successful');
+
+  try{
+    const res1 = await fetch("/api/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        full_name, username, password
+      })
+    })
+
+    const data1 = await res1.json()
+
+    if(!res1.ok){
+        throw data1.message
+    }
+
+    const res2 = await fetch("/api/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        username, password
+      })
+    })
+
+    const data2 = await res2.json()
+
+    if(!res2.ok){
+        throw data2.message
+    }
+
+    location.assign("/")
+    
+  }catch(err){
+      console.log(err)
+      activateAndDeactivateAlert(err, "error")
+  }
 });
-  
